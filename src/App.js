9e4b2c1635cb188a5408d48edf4a61b0a62fc5e4
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-12-05 22:57:52 +0530

    installed emotion react and emotion styled for mui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+
+// / -> root -> all movies
+// /movie1 -> movie information -> more
 
 function App() {
-  return <div className="App">hello worlddsdfasdfad</div>;
+  return (
+    <div>
+      <CssBaseline />
+      <h1>Hello World Tibet Film</h1>
+    </div>
+  );
 }
 
 export default App;
