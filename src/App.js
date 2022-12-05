0ebc476fa40f998e2dd5ef95cb5cbc0543c57271
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-12-05 23:52:03 +0530

    basic routing done using react router dom version 6

--- src/App.js
+++ src/App.js
@@ -9,7 +9,15 @@ function App() {
   return (
     <div>
       <CssBaseline />
-      <h1>Hello World Tibet Film</h1>
+      <main>
+        <Routes>
+          <Route path="/movie/:id" element={<h1>Movie Information</h1>} />
+          <Route path="/actors/:id" element={<h1>Actors</h1>} />
+          <Route path="/" element={<h1>Movies</h1>} />
+          <Route path="/profile/:id" element={<h1>Profile</h1>} />
+        </Routes>
+      </main>
+      {/* <h1>Hello World Tibet Film</h1> */}
     </div>
   );
 }
