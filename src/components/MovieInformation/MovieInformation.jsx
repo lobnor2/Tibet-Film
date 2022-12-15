11b932c9b70fdd1fc15fd85eafa8f06606c70a8e
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-12-15 20:23:31 +0530

    done movie information with topcast name and pic with responsive design and added six buttons

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -9,6 +9,7 @@ import {
   CircularProgress,
   useMediaQuery,
   Rating,
+  Divider,
 } from "@mui/material";
 import {
   Movie as MovieIcon,
@@ -30,10 +31,19 @@ import genreIcons from "../../assets/genres";
 import { selectGenreOrCategory } from "../../features/currentGenreOrCategory";
 
 const MovieInformation = () => {
+  const isMobile = useMediaQuery("(max-Width:600px)");
+
   const { id } = useParams();
   const { data, isFetching, error } = useGetMovieQuery(id);
   const classes = useStyles();
   const dispatch = useDispatch();
+  // console.log(data);
+
+  const isMovieFavorited = true;
+  const isMovieWatchlisted = true;
+
+  const addToFavorites = () => {};
+  const addToWatchlist = () => {};
 
   if (isFetching) {
     return (
@@ -61,12 +71,35 @@ const MovieInformation = () => {
         />
       </Grid>
       <Grid item container direction="column" lg={6}>
-        <Typography variant="h3" align="center" gutterBottom>
-          {data?.title} ({data.release_date.split("-")[0]})
-        </Typography>
-        <Typography variant="h5" align="center" gutterBottom>
-          {data?.tagline}
-        </Typography>
+        {isMobile ? (
+          <Typography
+            variant="h4"
+            align="center"
+            gutterBottom
+            className={classes.title}
+          >
+            {data?.title} ({data.release_date.split("-")[0]})
+          </Typography>
+        ) : (
+          <Typography
+            variant="h3"
+            align="center"
+            gutterBottom
+            className={classes.title}
+          >
+            {data?.title} ({data.release_date.split("-")[0]})
+          </Typography>
+        )}
+        {isMobile ? (
+          <Typography variant="subtitle1" align="center" gutterBottom>
+            {data?.tagline}
+          </Typography>
+        ) : (
+          <Typography variant="h6" align="center" gutterBottom>
+            {data?.tagline}
+          </Typography>
+        )}
+
         <Grid item className={classes.containerSpaceAround}>
           <Box display="flex" align="center">
             <Rating readonly value={data.vote_average / 2} />
@@ -105,6 +138,148 @@ const MovieInformation = () => {
               </Typography>
             </Link>
           ))}
+        </Grid>
+        <Typography
+          variant="h5"
+          gutterBottom
+          style={{
+            marginTop: "10px",
+            marginLeft: "10px",
+          }}
+        >
+          Overview
+        </Typography>
+        <Typography sx={{ mx: "10px", mb: "30px" }}>
+          {data?.overview}
+        </Typography>
+        <Divider />
+        <Typography
+          variant="h5"
+          sx={{ mx: "10px", mb: "20px" }}
+          display="flex"
+          justifyContent="center"
+        >
+          Top Cast
+        </Typography>
+
+        <Grid item container spacing={3} style={{ paddingLeft: "10px" }}>
+          {data &&
+            data.credits.cast
+              .map(
+                (character, i) =>
+                  character.profile_path && (
+                    <>
+                      <Grid
+                        key={i}
+                        item
+                        sx={4}
+                        sm={3}
+                        md={2}
+                        component={Link}
+                        to={`/actors/${character.id}`}
+                        style={{ textDecoration: "none" }}
+                      >
+                        <img
+                          src={`https://image.tmdb.org/t/p/w500/${character.profile_path}`}
+                          alt={character.name}
+                          className={classes.castImage}
+                        />
+                        {isMobile ? (
+                          <Typography color="textPrimary" variant="body2">
+                            {character?.name}
+                          </Typography>
+                        ) : (
+                          <Typography color="textPrimary" variant="">
+                            {character?.name}
+                          </Typography>
+                        )}
+                        {isMobile ? (
+                          <Typography color="textSecondary" variant="body2">
+                            {character.character.split("/")[0]}
+                          </Typography>
+                        ) : (
+                          <Typography color="textSecondary">
+                            {character.character.split("/")[0]}
+                          </Typography>
+                        )}
+                      </Grid>
+                    </>
+                  )
+              )
+              .slice(0, 6)}
+        </Grid>
+        <Grid item container style={{ marginTop: "2rem" }}>
+          <div
+          // className={classes.buttonsContainer}
+          >
+            <Grid item xs={12} sm={6} className={classes.buttonsContainer}>
+              <ButtonGroup
+                size="medium"
+                variant="outlined"
+                fullWidth
+                sx={{ px: { xs: "15px" }, width: { lg: "400px" }, my: "10px" }}
+              >
+                <Button
+                  target="_blank "
+                  rel="noopener noreferrer"
+                  href={data?.homepage}
+                  endIcon={<Language />}
+                >
+                  Website
+                </Button>
+                <Button
+                  target="_blank "
+                  rel="noopener noreferrer"
+                  href={`https://www.imdb.com/title/${data?.imdb_id}`}
+                  endIcon={<MovieIcon />}
+                >
+                  IMDB
+                </Button>
+                <Button onClick={() => {}} href="#" endIcon={<Theaters />}>
+                  Trailer
+                </Button>
+                {/* {isMobile ? <br /> : ""} */}
+              </ButtonGroup>
+              <ButtonGroup
+                size="medium"
+                variant="outlined"
+                fullWidth
+                sx={{ px: { xs: "15px" }, width: { lg: "400px" } }}
+              >
+                <Button
+                  onClick={addToFavorites}
+                  endIcon={
+                    isMovieFavorited ? <Favorite /> : <FavoriteBorderOutlined />
+                  }
+                >
+                  {isMovieFavorited ? "Unfavorite" : "Favorite"}
+                </Button>
+                <Button
+                  onClick={addToWatchlist}
+                  endIcon={isMovieWatchlisted ? <Remove /> : <PlusOne />}
+                >
+                  {/* {isMovieWatchlisted
+                    ? "Remove from Watchlist"
+                    : "Add to Watchlist"} */}
+                  Watchlist
+                </Button>
+                <Button
+                  endIcon={<ArrowBack />}
+                  sx={{ borderColor: "primary.main" }}
+                >
+                  <Typography
+                    component={Link}
+                    to="/"
+                    color="inherit"
+                    variant="subtitle2"
+                    style={{ textDecoration: "none" }}
+                  >
+                    Back
+                  </Typography>
+                </Button>
+              </ButtonGroup>
+            </Grid>
+          </div>
         </Grid>
       </Grid>
     </Grid>
--- src/components/MovieInformation/style.js
+++ src/components/MovieInformation/style.js
@@ -4,25 +4,27 @@ export default makeStyles((theme) => ({
   containerSpaceAround: {
     display: "flex",
     justifyContent: "space-around",
+    maxWidth: "100vw",
     // margin: "10px 0",
-    [theme.breakpoints.down("sm")]: {
-      flexDirection: "column",
-      flexWrap: "wrap",
-    },
+    // [theme.breakpoints.down("sm")]: {
+    //   flexDirection: "column",
+    //   flexWrap: "wrap",
+    // },
   },
   poster: {
     borderRadius: "20px",
     boxShadow: "0.5em 0.5em 1em rgba(64,64,70,0.7)",
-    width: "80%",
+    width: "100%",
     [theme.breakpoints.down("sm")]: {
+      margin: "0 auto",
       width: "60%",
       height: "350px",
-      margin: "0 auto",
       marginBottom: "30px",
       display: "flex",
       justifyContent: "center",
     },
   },
+
   genresContainer: {
     display: "flex",
     justifyContent: "center",
@@ -34,9 +36,47 @@ export default makeStyles((theme) => ({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    textDecoration: "none",
   },
   genreImage: {
     filter: theme.palette.mode === "dark" && "invert(1)",
     marginRight: "3px",
   },
+
+  castImage: {
+    // border: "1px solid red",
+    width: "100%",
+    maxWidth: "7em",
+    height: "8em",
+    objectFit: "cover",
+    borderRadius: "10px",
+    // boxShadow: "2px 2px 4px 2px rgba(54,54,54,0.5)",
+    // display: "flex",
+    // justifyContent: "space-between",
+    // marginRight: "15px",
+    [theme.breakpoints.down("sm")]: {
+      width: "170px",
+      //   justifyContent: "center",
+      //   alignItems: "center",
+      //   flexWrap: "wrap",
+      height: "160px",
+      //   marginBottom: "10px",
+    },
+  },
+
+  //   buttonsContainer: {
+  //     display: "flex",
+  //     justifyContent: "space-between",
+  //     width: "100%",
+  //     marginRight: "20px",
+  //     [theme.breakpoints.down("sm")]: {
+  //       maxWidth: "100vw",
+  //       display: "flex",
+  //       border: "1px solid red",
+  //       justifyContent: "center",
+  //       alignItems: "center",
+  //       flexDirection: "column",
+  //     },
+  //   },
+  buttonsContainer: {},
 }));
