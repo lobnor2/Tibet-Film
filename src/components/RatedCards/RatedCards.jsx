7f28f24f5c769960017bd64d3af817a8da99f381
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-12-16 14:02:27 +0530

    changed few styles in mobile

--- src/components/RatedCards/RatedCards.jsx
+++ src/components/RatedCards/RatedCards.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import useStyles from "./styles";
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, useMediaQuery } from "@mui/material";
 import { Movie } from "..";
 
 const RatedCards = ({ title, data }) => {
   const classes = useStyles();
+  const isMobile = useMediaQuery("(max-Width:600px)");
   return (
     <Box>
-      <Typography variant="h5" gutterBottom>
-        {title}
-      </Typography>
+      {isMobile ? (
+        <Typography
+          variant="h5"
+          gutterBottom
+          sx={{ mt: "30px", mb: "-10px" }}
+          align="center"
+        >
+          {title}
+        </Typography>
+      ) : (
+        <Typography variant="h4" gutterBottom sx={{ mt: "30px", pl: "35px" }}>
+          {title}
+        </Typography>
+      )}
+
       <Box display="flex" flexWrap="wrap" className={classes.container}>
         {data?.results.map((movie, i) => (
           <Movie key={movie.id} movie={movie} i={i} />
--- src/components/RatedCards/styles.js
+++ src/components/RatedCards/styles.js
@@ -1,3 +1,12 @@
 import { makeStyles } from "@mui/styles";
 
-export default makeStyles((theme) => ({}));
+export default makeStyles((theme) => ({
+  container: {
+    [theme.breakpoints.down("sm")]: {
+      //   border: "1px solid red",
+      display: "flex",
+      justifyContent: "space-evenly",
+      alignItems: "center",
+    },
+  },
+}));
