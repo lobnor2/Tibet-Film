c8440a11131cea0ae2336225cb20304f75057426
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-12-17 00:33:35 +0530

    error fixed for double routing name problem

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -2,14 +2,27 @@ import React, { useContext, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import { ColorModeContext } from "../utils/ToggleColorMode";
 import { fetchToken } from "../utils";
+import { selectGenreOrCategory } from "../features/currentGenreOrCategory";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   useEffect(() => {
     alanBtn({
       key: "dd657cf801cd59dc48895e1da0e0aedf2e956eca572e1d8b807a3e2338fdd0dc/stage",
-      onCommand: ({ command, mode }) => {
-        if (command === "changeMode") {
+      onCommand: ({ command, mode, genres, genreOrCategory }) => {
+        if (command === "chooseGenre") {
+          const foundGenre = genres.find(
+            (g) => g.name.toLowerCase() === genreOrCategory.toLowerCase()
+          );
+          if (foundGenre) {
+            navigate("/");
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          }
+        } else if (command === "changeMode") {
           if (mode === "light") {
             setMode("light");
           } else {
@@ -19,8 +32,8 @@ const useAlan = () => {
           fetchToken();
         } else if (command === "logout") {
           localStorage.clear(); //will clear the local Storage
-
-          window.location.href = "/";
+          navigate("/");
+          //   window.location.href = "/";
         }
       },
     });
--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { Typography, Grid, Grow, Tooltip, Rating } from "@mui/material";
+import {
+  Typography,
+  Grid,
+  Grow,
+  Tooltip,
+  Rating,
+  useMediaQuery,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 import useStyles from "./styles";
-import { useMediaQuery } from "@mui/material";
 
 const Movie = ({ movie, i }) => {
   const isMobile = useMediaQuery("(max-Width:600px)");
@@ -10,7 +16,7 @@ const Movie = ({ movie, i }) => {
   return (
     <Grid item sx={6} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
       <Grow in key={i} timeout={(i + 1) * 250}>
-        <Link className={classes.links} to={`movie/${movie.id}`}>
+        <Link className={classes.links} to={`/movie/${movie.id}`}>
           <img
             alt={movie.title}
             className={classes.image}
