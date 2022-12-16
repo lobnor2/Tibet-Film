a5bee26f2a335852e10da9d49f4679029f5b89f6
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-12-16 13:45:57 +0530

    rendered fav and watchlist in the profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,11 +4,30 @@ import { useSelector } from "react-redux";
 import { ExitToApp } from "@mui/icons-material";
 import { userSelector } from "../../features/auth.js";
 import useStyles from "./style.js";
+import { useGetListQuery } from "../../services/TMDB.js";
+import { RatedCards } from "..";
 
 const Profile = () => {
   const { user } = useSelector(userSelector);
+  const { data: favoriteMovies, refetch: refetchFavorites } = useGetListQuery({
+    listName: "favorite/movies",
+    accountId: user.id,
+    sessionId: localStorage.getItem("session_id"),
+    page: 1,
+  });
+  const { data: watchlistMovies, refetch: refetchWatchlist } = useGetListQuery({
+    listName: "watchlist/movies",
+    accountId: user.id,
+    sessionId: localStorage.getItem("session_id"),
+    page: 1,
+  });
+  useEffect(() => {
+    refetchFavorites();
+    refetchWatchlist();
+  }, []);
+
   const classes = useStyles();
-  const favoriteMovies = [];
+  // const favoriteMovies = [];
   const logout = () => {
     localStorage.clear(); //will clear the local Storage
 
@@ -27,12 +46,15 @@ const Profile = () => {
           My Profile {`(${user.username})`}
         </Typography>
       </Box>
-      {!favoriteMovies.length ? (
+      {!favoriteMovies?.results?.length && !watchlistMovies?.results?.length ? (
         <Typography variant="h5" style={{ textAlign: "center" }}>
           Add Favorite Movies
         </Typography>
       ) : (
-        <Box>Favorite Movies</Box>
+        <Box>
+          <RatedCards title="Favorite Movies" data={favoriteMovies} />
+          <RatedCards title="Watchlist Movies" data={watchlistMovies} />
+        </Box>
       )}
 
       <Button
